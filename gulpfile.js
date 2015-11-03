50eabb4b56e8694cbb792235db49f3ebf3116bac
Vladimir Simonov <slmgc@ya.ru>
2015-11-03 11:54:54 +0300

    Fixed css watcher Removed redundant media

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
+'use strict';
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
+const plumber = require('gulp-plumber');
 const livereload = require('gulp-livereload');
 
 
 gulp
-	.task('watch:css', () => {
+	.task('watch:css', ['build:css'], () => {
 		livereload.listen();
 		gulp.watch('assets/**/*.styl', ['build:css']);
 	})
 
-	.task('build:css', () => {
+	.task('build:css', () =>
 		gulp.src('assets/bundle.styl')
+			.pipe(plumber())
 			.pipe(stylus())
 			.pipe(gulp.dest('public'))
 			.pipe(livereload())
-	});
+	);
