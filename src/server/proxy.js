9db9c6ea2cfd18fbe60ed112bab48df442ea557c
Vladimir Simonov <slmgc@ya.ru>
2016-03-17 12:39:28 +0300

    Fixed proxy

--- src/server/proxy.js
+++ src/server/proxy.js
@@ -1,5 +1,14 @@
+const {STATUS_CODES} = require('http')
+
+
 module.exports = require('express')()
 	.use('/proxy', (req, res) => {
 		const url = `http://jsonplaceholder.typicode.com${req.url}`
-		req.pipe(request(url)).pipe(res.type('json'))
+
+		req.pipe(request(url).on('error', (e) => {
+			const status = 503
+			const message = STATUS_CODES[status]
+
+			res.status(status).json({error: {status, message}})
+		})).pipe(res.type('json'))
 	})
